03a71ecc83d7853762607d4c7e148df472d352bc
hiroko4ikeda8 <art_stylist_spinning@yahoo.co.jp>
2026-08-12 21:25:24 +0900

    feat: ルーティングを/snsに変更しナビゲーションのdashboard参照を修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,9 +18,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/sns', function() {
+    return view('sns.index');
+})->middleware(['auth', 'verified'])->name('sns');
+
+//Route::get('/dashboard', function () {
+  //  return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
